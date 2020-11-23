72a85876d2ceb6820b1df9e63bb467d4088d59e5
sky0621 <sky14work@gmail.com>
2020-11-24 00:28:55 +0900

    front

--- try02/frontend/components/TodoForm.vue
+++ try02/frontend/components/TodoForm.vue
@@ -1,11 +1,77 @@
 <template>
-  <v-container></v-container>
+  <v-container>
+    <ValidationObserver
+      ref="observer"
+      v-slot="{ invalid }"
+      @submit.prevent="submit"
+      slim
+    >
+      <v-form>
+        <v-row>
+          <v-col cols="5">
+            <v-card>
+              <ValidationProvider
+                v-slot="{ errors }"
+                name="TODO"
+                rules="required|min:4|max:10"
+              >
+                <v-text-field
+                  v-model="todoInput.text"
+                  :error-messages="errors[0]"
+                  label="TODO"></v-text-field>
+              </ValidationProvider>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="5">
+            <v-card>
+              <ValidationProvider
+                v-slot="{ errors }"
+                name="ユーザーID"
+                rules="required"
+              >
+                <v-text-field
+                  v-model="todoInput.userId"
+                  :error-messages="errors[0]"
+                  label="ユーザーID"></v-text-field>
+              </ValidationProvider>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="5">
+            <v-btn @click="submit">登録</v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
+    </ValidationObserver>
+  </v-container>
 </template>
 <script lang="ts">
 import { Component, Vue } from '~/node_modules/nuxt-property-decorator'
-// eslint-disable-next-line no-unused-vars
-// import createTodo from '~/apollo/mutation/todo.gql'
+import { ValidationObserver } from 'vee-validate'
+import { NewTodo } from '~/gql-types'
+
+export class TodoInput implements NewTodo {
+  text: string = ''
+  userId: string = ''
+}
 
 @Component({})
-export default class TodoForm extends Vue {}
+export default class TodoForm extends Vue {
+  $refs!: {
+    observer: InstanceType<typeof ValidationObserver>
+  }
+
+  todoInput: NewTodo = new TodoInput()
+
+  async submit() {
+    console.log('Submit!')
+    const valid = await this.$refs.observer.validate()
+    if (valid) {
+      console.log('valid!!!')
+    }
+  }
+}
 </script>
--- try02/frontend/plugins/vee-validate.ts
+++ try02/frontend/plugins/vee-validate.ts
@@ -1,4 +1,14 @@
-import { extend } from 'vee-validate'
-import { required } from 'vee-validate/dist/rules'
+import Vue from 'vue'
+import {
+  extend,
+  ValidationObserver,
+  ValidationProvider
+} from 'vee-validate'
+import { max, min, required } from 'vee-validate/dist/rules'
+
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
 
 extend('required', required)
+extend('min', min)
+extend('max', max)
